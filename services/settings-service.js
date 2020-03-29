dd7e1eec1ae6ea010c6bb6629ad182efb676dba1
bitglimpse <bitglimpse@gmail.com>
2020-03-29 14:25:16 -0600

    clear settings service

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,24 +1,14 @@
-const fs = require("fs");
-const path = require("path");
-
 function getSettings() {
   return {}
 }
 
 function writeSettings(newSettings) {
-  return false
 }
 
 function getDefaultDir() {
-  const defaultDir = getSettings().defaultDir
-  if (!defaultDir) {
-    return process.cwd()
-  }
-  return isValidDir(defaultDir) ? defaultDir : process.cwd()
 }
 
 function isValidDir(dirPath) {
-  return false
 }
 
 module.exports = {
