176bd7f492bd0773dcd5b77051b0a5c0cbc40021
bitglimpse <bitglimpse@gmail.com>
2020-04-08 08:11:23 -0600

    remove space from test tag

--- test/module4/routes-add-query-param.spec.js
+++ test/module4/routes-add-query-param.spec.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 describe("routes/index.js", () => {
-  it('`routes/index.js` should exist  @routes-add-query-param', () => {
+  it('`routes/index.js` should exist @routes-add-query-param', () => {
     assert(fs.existsSync(path.join(process.cwd(), 'routes')), 'The `routes` dir does not exist.');
     assert(
       fs.existsSync(path.join(process.cwd(), 'routes/index.js')),
